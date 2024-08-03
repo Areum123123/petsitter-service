24037371aac189795c232cd6427cc1cb2062a5d5
areum <kang10232@naver.com>
2024-08-03 18:34:50 +0900

    feat:토큰재발급 API

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -77,12 +77,85 @@ authRouter.post('/sign-in', signInValidator, async (req, res, next) => {
       ACCESS_TOKEN_SECRET_KEY,
       { expiresIn: '12h' },
     );
-    res.header('accessToken', accessToken);
+
+    //refresh토큰  발급
+    const refreshToken = jwt.sign(
+      {
+        id: user.id,
+      },
+      REFRESH_TOKEN_SECRET_KEY,
+      { expiresIn: '7d' },
+    );
+
+    //refresh 토큰 저장(hash 값으로 저장)
+    const hashedRefreshToken = bcrypt.hashSync(refreshToken, 10);
+
+    //refresh 토큰 생성 또는 갱신 upsert() 있으면 update 없으면 create
+    await prisma.refresh_tokens.upsert({
+      where: {
+        user_id: user.id,
+      },
+      update: {
+        refresh_token: hashedRefreshToken,
+      },
+      create: {
+        user_id: user.id,
+        refresh_token: hashedRefreshToken,
+      },
+    });
 
     return res.status(200).json({
       status: 200,
       message: '로그인 성공했습니다.',
       accessToken: accessToken,
+      refreshToken: refreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+//토큰 재발급 API
+
+authRouter.post('/token', requireRefreshToken, async (req, res, next) => {
+  //RefreshToken**(JWT)을 **Request Header의 Authorization** 값(**`req.headers.authorization`**)으로 전달 받습니다.
+  //사용자 정보는 **인증 Middleware(`req.user`)**를 통해서 전달 받습니다.
+  const userId = req.user.id;
+
+  try {
+    // 2. **비즈니스 로직(데이터 처리)**
+    //     - **AccessToken(Payload**에 **`사용자 ID`**를 포함하고, **유효기한**이 **`12시간`)**을 생성합니다.
+    //     - **RefreshToken** (**Payload**: **사용자 ID** 포함, **유효기한**: **`7일`**)을 생성합니다.
+    const payload = { userId: userId };
+    // AccessToken(Payload**에 **`사용자 ID`**를 포함하고, **유효기한**이 **`12시간`)**을 생성합니다.
+    const accessToken = jwt.sign(payload, ACCESS_TOKEN_SECRET_KEY, {
+      expiresIn: '12h',
+    });
+
+    const refreshToken = jwt.sign(payload, REFRESH_TOKEN_SECRET_KEY, {
+      expiresIn: '7d',
+    });
+
+    const hashedRefreshToken = bcrypt.hashSync(refreshToken, 10);
+    //     - DB에 저장 된 **RefreshToken을 갱신**합니다.
+    // 3. **반환 정보**
+    //     - **AccessToken, RefreshToken**을 반환합니다.
+    await prisma.refresh_tokens.upsert({
+      where: { user_id: +userId },
+      update: {
+        refresh_token: hashedRefreshToken,
+      },
+      create: {
+        user_id: +userId,
+        refresh_token: hashedRefreshToken,
+      },
+    });
+
+    return res.status(200).json({
+      status: 200,
+      message: '토큰 재발급에 성공했습니다.',
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     });
   } catch (err) {
     next(err);
