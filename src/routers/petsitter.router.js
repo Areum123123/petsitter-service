2e16550e4bc2b5399674aaa3300513040c6d4104
areum <kang10232@naver.com>
2024-08-06 09:17:59 +0900

    feat:평점작성시 펫시터 평점업데이트 기능 추가

--- src/routers/petsitter.router.js
+++ src/routers/petsitter.router.js
@@ -21,39 +21,63 @@ petsitterRouter.post(
       const { petSitterId } = req.params;
       const { rating, comment } = req.body;
 
-      const petSitter = await prisma.petsitters.findUnique({
-        where: { id: +petSitterId },
-      });
-      if (!petSitter) {
-        return res.status(404).json({
-          status: 404,
-          message: '펫시터를 찾을 수 없습니다',
+      const transactionResult = await prisma.$transaction(async (prisma) => {
+        const petSitter = await prisma.petsitters.findUnique({
+          where: { id: +petSitterId },
         });
-      }
-      //리뷰생성
-      const review = await prisma.reviews.create({
-        data: {
-          user_id: +userId,
-          pet_sitter_id: +petSitterId,
-          rating,
-          comment,
-        },
-        include: {
-          users: {
-            select: { name: true },
+        if (!petSitter) {
+          return res.status(404).json({
+            status: 404,
+            message: '펫시터를 찾을 수 없습니다',
+          });
+        }
+        //리뷰생성
+        const review = await prisma.reviews.create({
+          data: {
+            user_id: +userId,
+            pet_sitter_id: +petSitterId,
+            rating,
+            comment,
           },
-        },
+          include: {
+            users: {
+              select: { name: true },
+            },
+          },
+        });
+
+        //펫시터 리뷰 평점 업데이트 코드시작
+        // 모든 리뷰 조회 및 평균 계산
+        const reviews = await prisma.reviews.findMany({
+          where: { pet_sitter_id: +petSitterId },
+        });
+
+        const averageRating = (
+          reviews.reduce((sum, review) => sum + review.rating, 0) /
+          reviews.length
+        ).toFixed(1);
+
+        // 펫시터의 total_rate 업데이트
+        await prisma.petsitters.update({
+          where: { id: +petSitterId },
+          data: { total_rate: +averageRating },
+        });
+
+        return {
+          review,
+          //averageRating,
+        };
       });
 
       const formattedReview = {
-        review_id: review.id,
-        pet_sitter_id: review.pet_sitter_id,
+        review_id: transactionResult.review.id,
+        pet_sitter_id: transactionResult.review.pet_sitter_id,
         reviews: {
-          user_name: review.users.name,
-          rating: review.rating,
-          comment: review.comment,
-          created_at: review.created_at,
-          updated_at: review.updated_at,
+          user_name: transactionResult.review.users.name,
+          rating: transactionResult.review.rating,
+          comment: transactionResult.review.comment,
+          created_at: transactionResult.review.created_at,
+          updated_at: transactionResult.review.updated_at,
         },
       };
       return res.status(201).json({
